c071be30383a2e2cea470d8728563c00c7f2ceda
Piyushh-h <piyushsinha0810@gmail.com>
2025-01-31 19:40:37 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // GitHub Pages does not support Next.js image optimization
+  },
+  basePath: "/piyushportf", // Required for GitHub Pages
+  assetPrefix: "/piyushportf/",
 };
 
 export default nextConfig;
